a1333df2c93c82438a51a8a3121c8a161420eb97
Alex Katsero <alexanderkatsero@gmail.com>
2020-12-13 12:21:32 +0200

    feat: accept environment value from input parameters

--- check.js
+++ check.js
@@ -1,9 +1,15 @@
 #!/usr/bin/env node
 
+const yargs = require('yargs/yargs')
 const verify = require('./lib/verify')
 
 async function main() {
   const stdin = process.openStdin()
+
+  const argv = yargs(process.argv).argv
+  if (!argv.env) {
+    console.warn('Environment is not provided. Assuming production.')
+  }
 
   let data = ''
 
@@ -11,7 +17,7 @@ async function main() {
     data += chunk
   })
 
-  stdin.on('end', () => verify(data))
+  stdin.on('end', () => verify(data, argv.env || 'production'))
 }
 
-main()
+main().catch(e => console.error(e))
